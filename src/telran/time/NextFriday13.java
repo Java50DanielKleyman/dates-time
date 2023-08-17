e44765b426536847e28551095af7a21a482ce2a3
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-17 14:15:50 +0300

    HW-24

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -14,10 +14,27 @@ public class NextFriday13 implements TemporalAdjuster {
 		if (!temporal.isSupported(ChronoUnit.YEARS)) {
 			throw new UnsupportedTemporalTypeException("must support years");
 		}
+
+		if (temporal.get(ChronoField.DAY_OF_MONTH) == 13) {
+			return temporal = monthPlus(temporal);
+		}
+		;
 		do {
 			temporal = temporal.plus(1, ChronoUnit.DAYS);
-		} while (temporal.get(ChronoField.DAY_OF_WEEK) != 5 | temporal.get(ChronoField.DAY_OF_MONTH) != 13);
+		} while (temporal.get(ChronoField.DAY_OF_MONTH) != 13);
+		if (temporal.get(ChronoField.DAY_OF_WEEK) == 5) {
+			return temporal;
+		} else {
+			temporal = monthPlus(temporal);
+		}
+
 		return temporal;
 	}
 
+	private Temporal monthPlus(Temporal temporal) {
+		do {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		} while (temporal.get(ChronoField.DAY_OF_WEEK) != 5);
+		return temporal;
+	}
 }
